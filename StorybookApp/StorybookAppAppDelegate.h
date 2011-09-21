b0b9df324ff26d4edc28dfe1aecbc72890496ca0
Muhammad Azeem <iphonedevc@Muhammads-Mac.local>
2011-09-21 20:36:12 +0500

    Initial Commit

---
+++ StorybookApp/StorybookAppAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  StorybookAppAppDelegate.h
+//  StorybookApp
+//
+//  Created by Muhammad Azeem on 9/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StorybookAppAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
